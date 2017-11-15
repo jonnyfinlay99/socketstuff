7f2fb153a6803e2f23ea01e3c47606421fe31896
Jonathan <jonathan@origin-digital.com>
2017-11-15 11:56:14 +0000

    finished tester Android

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { BehaviorSubject } from "rxjs/Rx";
 import CryptoJS from 'crypto-js';
+import { AlertController } from 'ionic-angular';
+
 import aesjs from 'aes-js';
-import pkcs7 from 'pkcs7';
+// import pkcs7 from 'pkcs7';
 
 declare var chrome;
+declare var Socket;
 declare var CryptoJS;
 declare var aesjs;
-declare var pkcs7;
+// declare var pkcs7;
 
 @Component({
   selector: 'page-home',
@@ -19,38 +22,64 @@ export class HomePage {
   private socketid: number;
   private localAddress: string;
 
-  constructor(public navCtrl: NavController) {
+  private wifi = {
+    name: '',
+    password: ''
+  }
+  private encryptedInfo;
+  private socket;
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+
 
   }
-  doTCPSHIT() {
-    console.log('After sendTCP');
+  successAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Wifi-Pairing',
+      subTitle: 'Wifi has been successfully paired to the device',
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
 
-    this.sendTCPMessage('', '192.168.1.1', 80)
-      .subscribe((stuff) => { console.log('stuff', stuff); });
+  errorAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Wifi-Pairing',
+      subTitle: 'Wifi has been unable to pair with the device',
+      buttons: ['Dismiss']
+    });
+    alert.present();
   }
 
 
+  doTcpConnection(encryptedWifiDetails) {
+    //console.log('After sendTCP');
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ');
-    // server IP = 192.168.1.1
-    // mask = 255.255.255.0
-    // port = 80
+    this.sendTCPMessage(encryptedWifiDetails, '192.168.1.1', 80)
+      .subscribe((stuff) => {
+        //console.log('stuff', stuff);
+      });
+  }
 
-    function padString(source) {
-      var paddingChar = ' ';
-      var size = 16;
-      var padLength = size - source.length;
+  padString(source) {
+    var paddingChar = ' ';
+    var size = 16;
+    var padLength = size - source.length;
 
-      for (var i = 0; i < padLength; i++) source += paddingChar;
+    for (var i = 0; i < padLength; i++) source += paddingChar;
 
-      return source;
-    }
+    return source;
+  }
+
+  doCryptoStuff(wifiname, password) {
+
 
     var key = CryptoJS.enc.Hex.parse('1b6e151628aed2a6abf7158809cf3f2c');
     var iv = CryptoJS.enc.Hex.parse('0000000000000000');
-    var message = 'ssid=“MyNetworkName”pwd=“MySecretPassword”';
-    var padMsg = padString(message);
+    //    var message = 'ssid=“MyNetworkName”pwd=“MySecretPassword”';
+
+    var message = 'ssid="' + wifiname + '"pwd="' + password + '"';
+    // var message = 'ssid="OriginDigitalDownstairs2"pwd="Gnat-Antelope42"';
+    var padMsg = this.padString(message);
 
     var encrypted = CryptoJS.AES.encrypt(padMsg, key, {
       iv: iv,
@@ -58,8 +87,8 @@ export class HomePage {
       mode: CryptoJS.mode.ECB
     });
 
-    console.log('encrypted text is ' + encrypted);
-    console.log('encrypted ciphertext is ' + encrypted.ciphertext);
+    //console.log('encrypted text is ' + encrypted);
+    //console.log('encrypted ciphertext is ' + encrypted.ciphertext);
 
     // Decrypt 
     var bytes = CryptoJS.AES.decrypt(encrypted.toString(), key, {
@@ -68,103 +97,215 @@ export class HomePage {
       mode: CryptoJS.mode.ECB
     });
     var plaintext = bytes.toString(CryptoJS.enc.Utf8);
-    console.log('Decryption : ' + plaintext);
+    //console.log('Decryption : ' + plaintext);
 
+
+
+    this.encryptedInfo = encrypted.ciphertext.toString();
+    this.doTcpConnection(encrypted);
 
 
   }
 
+
+  ionViewDidLoad() {
+    //console.log('ionViewDidLoad ');
+
+    var testString = 'c61f3d4b59da2b394eab989b69b29be10feafc832fb1d9c9679ec9165465ecf4';
+
+
+    var testString2 = "717ec305a9ff4197c42680b610b5461901014ce29404ee5d3628673b5eab43af992e2b51952e4ed5e62770d64e2bbd3a06a1e01ca4607af296d7c4d15de3c5eb";
+    // console.log(this.hexStringToByte(testString));
+    // console.log(this.byteToHexString(this.hexStringToByte(testString)));
+    // this.errorAlert();
+
+    //console.log('We will do the length of their wifi then ours');
+    // console.log(testString.length);
+    // console.log(testString2.length);
+
+    // console.log('What is their wifi string as a byte array');
+    // console.log(this.hexStringToByte(testString));
+    //console.log('What is our wifi string as a byte array');
+    // console.log(this.hexStringToByte(testString2));
+    //console.log(this.hexStringToByteArrayV2(testString2));
+    // console.log(this.hexStringToByte(testString2).buffer);
+    // console.log(this.hexStringToByteArrayV3(testString2));
+
+  }
+
+  ab2str2(buf) {
+    return String.fromCharCode.apply(null, new Uint16Array(buf));
+  };
+  hexStringToByteArrayV3(str) {
+
+    var a = []
+
+    if (Math.floor(str.length % 2) == 0) {
+      for (var i = 0; i < str.length; i = i + 2) {
+        var hex = str.substr(i, 2).toUpperCase();
+        // console.log(hex);
+        var number = parseInt(hex, 16);
+        a.push(number);
+
+      }
+    }
+
+    return new Uint16Array(a);
+  }
+  hexStringToByteArrayV2(str) {
+
+    var a = []
+
+    if (Math.floor(str.length % 2) == 0) {
+      for (var i = 0; i < str.length; i = i + 2) {
+        var hex = str.substr(i, 2).toUpperCase();
+        // console.log(hex);
+        var number = parseInt(hex, 16);
+
+
+        a.push(hex);
+
+      }
+    }
+
+    return new Uint16Array(a);
+  }
+
+  hexStringToByte(str) {
+
+    var a = [];
+    for (var i = 0, len = str.length; i < len; i += 2) {
+      a.push(parseInt(str.substr(i, 2), 16));
+    }
+
+    return new Uint8Array(a);
+  }
+  testFunction(str) {
+    var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i = 0, strLen = str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+  }
+
+
+  byteToHexString(uint8arr) {
+    if (!uint8arr) {
+      return '';
+    }
+
+    var hexStr = '';
+    for (var i = 0; i < uint8arr.length; i++) {
+      var hex = (uint8arr[i] & 0xff).toString(16);
+      hex = (hex.length === 1) ? '0' + hex : hex;
+      hexStr += hex;
+    }
+
+    return hexStr;
+  }
+
+  str2arrayBuffer(str) {
+    var buf = new ArrayBuffer(str.length);
+    var bufView = new Uint8Array(buf);
+    for (var i = 0; i < str.length; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+  }
+
+  str2ab(str) {
+    var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i = 0, strLen = str.length; i < strLen; i++) {
+      bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+  }
+  stringToArrayBuffer(string) {
+    var arrayBuffer = new ArrayBuffer(string.length * 2);
+    var buffer = new Uint8Array(arrayBuffer);
+    for (var i = 0, stringLength = string.length; i < stringLength; i++) {
+      buffer[i] = string.charCodeAt(i);
+      // Was: buffer = string.charCodeAt(i);
+    }
+    return buffer;
+  }
+
+  ab2str(buf) {
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
+  };
+
   sendTCPMessage(message2: string, address: string, port: number) {
+    // '192.168.1.1', 80
+    // let IP = "192.168.0.65";
+    // let Port = 80
+    let IP = "192.168.1.1";
+    let Port = 80
+    //console.log('Button');
 
-    // convert string to ArrayBuffer - taken from Chrome Developer page
-    function str2ab(str) {
+    var delay = 25000;	/// 5 seconds timeout
+    (<any>window).chrome.sockets.tcp.create({}, createInfo => { //callback function with createInfo as the parameter
+      var _socketTcpId = createInfo.socketId;
+      //console.log('CREATE SOCKET');
+      (<any>window).chrome.sockets.tcp.connect(_socketTcpId, IP, Port, result => { //callback function with result as the parameter
+        //console.log(result);
+        //console.log('What is the result');
+        if (result === 0) {
+          //console.log('What is encryptedInfo');
+          //console.log(this.encryptedInfo);
+          let message = this.str2ab(this.encryptedInfo);
+          var newMessage = this.hexStringToByte(this.encryptedInfo);
 
-      console.log("str", str);
+          /// connection ok, send the packet
+          (<any>window).chrome.sockets.tcp.onReceive.addListener(info => { //callback function with info as the parameter
+            /// recived, then close connection
+            console.log('onReceive ' + info);
+            (<any>window).chrome.sockets.tcp.close(_socketTcpId);
+            var data = this.ab2str(info.data);
+            console.log(data);
+            // this.successAlert();
 
-      var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-      var bufView = new Uint16Array(buf);
-      for (var i = 0, strLen = str.length; i < strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
-      }
-      return buf;
-    }
+            if (data.indexOf('ERR') >= 0) {
+              this.errorAlert();
 
-    function ab2str(buf) {
-      return String.fromCharCode.apply(null, new Uint8Array(buf));
-    };
+            } else {
+              this.successAlert();
+
+            }
+
+          });
+          chrome.sockets.tcp.setPaused(_socketTcpId, false);
+
+          // console.log('What is the message before being sent');
+          // console.log(newMessage.buffer);
+          (<any>window).chrome.sockets.tcp.send(_socketTcpId, newMessage.buffer, ((info) => {
+            // console.log('SENDING THE PACKET UP');
+            // console.log(info);
 
 
-    console.log('Received ip', address);
-
-    // only do udp stuff if there is plugin defined
-    if (typeof chrome.sockets !== 'undefined') {
-      console.log('CHROME SOCKETS IS AVAILABLE');
-      var subscribeURL = 'http://192.168.178.22:1400/MediaRenderer/GroupRenderingControl/Event';
-
-      // register the listeners
-      chrome.sockets.tcp.onReceive.addListener(
-        (info) => {
-          // we have found one 
-          console.log('Recv from socket: ', info, ab2str(info.data));
-          this.tcpstream.next(info);
+          }));
         }
-      );
-      // chrome.sockets.udp.onReceiveError.addListener(
-      //   (error) => {
-      //     console.log('Recv  ERROR from socket: ', error);
-      //     this.tcpstream.next({ 'error': error });
-      //   }
-      // );
+      });
 
-      // translate the string into ArrayBuffer
-      //  let SENDBUFFER = str2ab(message2);
+      /// set the timeout
+      setTimeout(function () {
+        (<any>window).chrome.sockets.tcp.close(_socketTcpId);
+      }, delay);
+    });
 
-      // send  the TCP MessageItem
-      chrome.sockets.tcp.create((createInfo) => {
-        this.socketid = createInfo.socketId;
-        console.log('Create socket ', createInfo, address, port);
 
-        chrome.sockets.tcp.connect(createInfo.socketId, '192.168.1.1', 80,
-          (resultCode) => {
-            console.log('START OF SOCKET CONNECTION');
-            if (resultCode < 0) { console.log('Error connect', resultCode) }
-            else chrome.sockets.tcp.getInfo(createInfo.socketId,
-
-              (getInfo) => {
-
-                console.log('Into getInfo', getInfo);
-
-                this.localAddress = getInfo.localAddress;
-
-                let strmessage = "Hello World";
-                let message = str2ab(strmessage);
-
-                chrome.sockets.tcp.send(this.socketid, message,
-                  (sendresult) => { console.log('Send result', sendresult) });
-              });
-          }
-        )
-      }
-      );
-
-      // and close the listener after a while
-      setTimeout(() => {
-        this.closeTCPService();
-      }, 10000);
-    }
-    // return the stream
     return this.tcpstream.asObservable().skip(1);
   }
 
-
-
-
-
-
   closeTCPService() {
-    // close the socket
-    if (typeof chrome.sockets !== 'undefined') chrome.sockets.tcp.disconnect(this.socketid);
 
+    //console.log('CLOSE THE TCP STREAM');
+    // close the socket
+    if (typeof chrome.sockets !== 'undefined') {
+
+      chrome.sockets.tcp.disconnect(this.socketid);
+    }
     // close the stream
     this.tcpstream.complete();
   }
